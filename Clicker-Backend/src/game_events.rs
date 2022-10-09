11b026dd006abf1e36195dbf91c7d86320ea3863
Marvin <msimon1@stud.hs-heilbronn.de>
2022-10-09 20:17:22 +0200

    chore: Add game events. it is now possible to create game events in rust and export them to typescript. #LABTROYALC-67 @author: Marvin

---
+++ Clicker-Backend/src/game_events.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+use typescript_type_def::TypeDef;
+
+#[derive(Deserialize, TypeDef)]
+pub enum ClientMessages {
+    //Insert events from frontend to backend
+}
+
+#[derive(Serialize, TypeDef)]
+pub enum ServerMessages {
+    //Insert events from backend to frontend
+    MineOre(u64),
+}
--- Clicker-Backend/src/main.rs
+++ Clicker-Backend/src/main.rs
@@ -1,3 +1,20 @@
+use crate::game_events::{ClientMessages, ServerMessages};
+
+mod game_events;
+
 fn main() {
-    println!("Hello, world!");
+    #[cfg(debug_assertions)]
+    {
+        use typescript_type_def::{write_definition_file, DefinitionFileOptions};
+        type Api = (ServerMessages, ClientMessages);
+        let ts_module = {
+            let mut buf = Vec::new();
+            let mut options = DefinitionFileOptions::default();
+            options.root_namespace = None;
+            write_definition_file::<_, Api>(&mut buf, options).unwrap();
+            String::from_utf8(buf).unwrap()
+        };
+
+        std::fs::write("../Clicker-Frontend/src/game_messages.ts", ts_module).unwrap();
+    }
 }

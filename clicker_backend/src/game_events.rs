65cb19db1571fc82c07ec8a698cea05834522be4
ntn98 <schmalfuss@stud.hs-heilbronn.de>
2022-10-15 15:09:17 +0200

    Add the shovel upgrade functionality Implemented the ClientMessage UpgradeShovel and the ServerMessage ShovelUpgraded in game_events.rs as well as the handling logic in game_state.rs #LABTROYALC-73 @author: Anton

--- clicker_backend/src/game_events.rs
+++ clicker_backend/src/game_events.rs
@@ -5,10 +5,12 @@ use typescript_type_def::TypeDef;
 pub enum ClientMessages {
     //Insert events from frontend to backend
     Mine,
+    UpgradeShovel,
 }
 
 #[derive(Serialize, TypeDef)]
 pub enum ServerMessages {
     //Insert events from backend to frontend
     NewState {ore: u64, depth: u64},
+    ShovelUpgraded {success: bool, new_level: i32},
 }
--- clicker_backend/src/game_state.rs
+++ clicker_backend/src/game_state.rs
@@ -1,3 +1,4 @@
+use std::thread::sleep;
 use crate::{ClientMessages, ServerMessages};
 use serde::{Deserialize, Serialize};
 
@@ -6,6 +7,7 @@ pub struct GameState {
     pub ore: u64,
     pub depth: u64,
     pub multiplier: u64,
+    pub shovel_level: i32,
 }
 
 impl GameState {
@@ -16,17 +18,27 @@ impl GameState {
     }
 
     /// Use this Function for Frontend -> Backend event handling
-    pub fn handle(&mut self, event: ClientMessages) {
+    pub fn handle(&mut self, event: ClientMessages){
+        let upgrade_costs = [50,  100, 200, 400, 800];
+        let max_level = 5;
         match event {
             // The depth is currently only increased once per event
             ClientMessages::Mine => {
-                self.depth += 1;
-                self.ore += 1;
+                self.depth += self.shovel_level as u64;
+                self.ore += self.shovel_level as u64;
+            }
+
+            ClientMessages:: UpgradeShovel => {
+                if self.shovel_level < max_level &&
+                    upgrade_costs[(self.shovel_level-1) as usize] <= self.ore {
+                    self.ore = self.ore - upgrade_costs[(self.shovel_level-1) as usize];
+                    self.shovel_level += 1;
+                }
             }
         }
     }
 
     pub fn new() -> Self {
-        GameState { ore: 0, depth: 0, multiplier: 0 }
+        GameState { ore: 0, depth: 0, multiplier: 0, shovel_level: 1 }
     }
 }

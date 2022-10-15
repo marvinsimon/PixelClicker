6356280e50a41408c8452ed22a5eb2cfa280cad2
mpereirakr <mpereirakr@stud.hs-heilbronn.de>
2022-10-15 16:06:46 +0200

    Added the PopUp for SignUp + solid-dismiss Implemented a rough PopUp with the special functionality to close automatically when clicked somewhere else. #LABTROYALC-77 @author: Moritz

--- clicker_frontend/src/App.tsx
+++ clicker_frontend/src/App.tsx
@@ -2,11 +2,14 @@ import type {Component} from 'solid-js';
 import {createSignal} from "solid-js";
 import styles from './App.module.css';
 import {ServerMessages, ClientMessages} from "./game_messages";
+import {Portal} from "solid-js/web";
+import Dismiss from "solid-dismiss";
 
 const App: Component = () => {
 
     const [ore, setOre] = createSignal(0);
-
+    const [open, setOpen] = createSignal(false);
+    let btnEl;
     let socket: WebSocket | undefined;
     const s = new WebSocket("ws://localhost:3001/game");
     const connectBackend = async () => {
@@ -36,15 +39,52 @@ const App: Component = () => {
         }
     }
 
+    const dropdown = () => {
+        const [open, setOpen] = createSignal(false);
+        let btnEl;
+
+        return (
+            <div style="position: relative;">
+                <button ref={btnEl}>Button</button>
+                <Dismiss
+                    menuButton={btnEl}
+                    open={open}
+                    setOpen={setOpen}
+                    cursorKeys
+                >
+                    <ul class="dropdown">
+                        <li><a href="#">cat</a></li>
+                        <li><a href="#">dog</a></li>
+                        <li><a href="#">fish</a></li>
+                    </ul>
+                </Dismiss>
+            </div>
+        );
+    };
+
     return (
         <div class={styles.App}>
             <header class={styles.header}>
+
+
                 <button class={styles.button} onClick={connectBackend}>Connect</button>
                 <button class={styles.button} onClick={disconnectBackend}>Disconnect</button>
                 <br/>
                 <button class={styles.button} onClick={click}>Mine Ore</button>
                 <br/>
                 <label>{ore()}</label>
+                <br/>
+                <button class={styles.button} ref={btnEl}>Popup</button>
+                <Dismiss
+                    menuButton={btnEl}
+                    open={open}
+                    setOpen={setOpen}
+                >
+                    <div class={styles.popup}>
+                        <p>Popup text!</p>
+                        <p>Lorem, <a href="#">ipsum</a> dolor.</p>
+                    </div>
+                </Dismiss>
             </header>
         </div>
     );

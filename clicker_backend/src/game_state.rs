9561962f01f21c07bd702f03becdbce1e6a66d05
ntn98 <schmalfuss@stud.hs-heilbronn.de>
2022-10-16 15:18:28 +0200

    Implemented the shovel upgrade functionality Changed main.rs to handle return ServerMessages from the handle function in game_state.rs and added a function to be returned after the UpgradeShovel event #LABTROYALC-73 @author: Anton

--- clicker_backend/src/game_state.rs
+++ clicker_backend/src/game_state.rs
@@ -18,21 +18,23 @@ impl GameState {
     }
 
     /// Use this Function for Frontend -> Backend event handling
-    pub fn handle(&mut self, event: ClientMessages){
+    pub fn handle(&mut self, event: ClientMessages) -> ServerMessages{
         let upgrade_costs = [50,  100, 200, 400, 800];
         let max_level = 5;
         match event {
-            // The depth is currently only increased once per event
             ClientMessages::Mine => {
                 self.depth += self.shovel_level as u64;
                 self.ore += self.shovel_level as u64;
+                ServerMessages::NewState { ore: self.ore, depth: self.depth }
             }
-
             ClientMessages:: UpgradeShovel => {
                 if self.shovel_level < max_level &&
                     upgrade_costs[(self.shovel_level-1) as usize] <= self.ore {
                     self.ore = self.ore - upgrade_costs[(self.shovel_level-1) as usize];
                     self.shovel_level += 1;
+                    ServerMessages::ShovelUpgraded{success: true, new_level: self.shovel_level}
+                } else {
+                    ServerMessages::ShovelUpgraded{success: false, new_level: self.shovel_level}
                 }
             }
         }

c65c8cd87327e8993e60290cf65e3b4d3cfd14fb
Anton Schmalfuss <schmalfuss@stud.hs-heilbronn.de>
2022-10-17 12:37:32 +0200

    Upgrade depth functionality
    changed logic to only upgrade the depth and adjusted variable names
    
    #LABTROAYC-73
    
    @author: Anton Schmalfuß

--- clicker_backend/src/game_state.rs
+++ clicker_backend/src/game_state.rs
@@ -7,7 +7,7 @@ pub struct GameState {
     pub ore: u64,
     pub depth: u64,
     pub multiplier: u64,
-    pub shovel_level: i32,
+    pub shovel_depth_level: i32,
 }
 
 impl GameState {
@@ -23,24 +23,24 @@ impl GameState {
         let max_level = 5;
         match event {
             ClientMessages::Mine => {
-                self.depth += self.shovel_level as u64;
-                self.ore += self.shovel_level as u64;
+                self.depth += self.shovel_depth_level as u64;
+                self.ore += 1;
                 ServerMessages::NewState { ore: self.ore, depth: self.depth }
             }
             ClientMessages:: UpgradeShovel => {
-                if self.shovel_level < max_level &&
-                    upgrade_costs[(self.shovel_level-1) as usize] <= self.ore {
-                    self.ore = self.ore - upgrade_costs[(self.shovel_level-1) as usize];
-                    self.shovel_level += 1;
-                    ServerMessages::ShovelUpgraded{success: true, new_level: self.shovel_level}
+                if self.shovel_depth_level < max_level &&
+                    upgrade_costs[(self.shovel_depth_level-1) as usize] <= self.ore {
+                    self.ore = self.ore - upgrade_costs[(self.shovel_depth_level-1) as usize];
+                    self.shovel_depth_level += 1;
+                    ServerMessages::ShovelDepthUpgraded{success: true, new_level: self.shovel_depth_level}
                 } else {
-                    ServerMessages::ShovelUpgraded{success: false, new_level: self.shovel_level}
+                    ServerMessages::ShovelDepthUpgraded{success: false, new_level: self.shovel_depth_level}
                 }
             }
         }
     }
 
     pub fn new() -> Self {
-        GameState { ore: 0, depth: 0, multiplier: 0, shovel_level: 1 }
+        GameState { ore: 0, depth: 0, multiplier: 0, shovel_depth_level: 1 }
     }
 }

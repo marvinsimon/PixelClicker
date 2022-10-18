2483b88225383d5c4a02004cb44c728bc4f2262c
Sergej Bakanow <sbakanow@stud.hs-heilbronn.de>
2022-10-18 19:30:14 +0200

    Add removal of old docker added the removal of an existing docker container before starting a fresh docker container #LABTROYALC-80 @author Sergej

--- clicker_backend/postgres/create.sh
+++ clicker_backend/postgres/create.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+docker stop royal-db
+docker rm royal-db
 sql=$(cat create_tables.sql)
 docker run -d -p 5432:5432 --name royal-db -e POSTGRES_PASSWORD=clickerroyale -e POSTGRES_USER=admin postgres:14.5-alpine postgres
 sleep 5

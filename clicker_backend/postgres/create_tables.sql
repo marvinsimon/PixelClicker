3ecdce457a37a272066982253e05fc13ca2a4d3b
Sergej Bakanow <sbakanow@stud.hs-heilbronn.de>
2022-10-25 21:49:23 +0200

    Finish Sign Up feature finished sign up feature in backend. #LABTROYALC-80 @author Sergej

--- clicker_backend/postgres/create_tables.sql
+++ clicker_backend/postgres/create_tables.sql
@@ -1,9 +1,9 @@
-CREATE EXTENSION citext;
+CREATE EXTENSION IF NOT EXISTS citext;
 CREATE DOMAIN email AS citext
     CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
 CREATE TABLE  Player (
     id BIGSERIAL PRIMARY KEY,
-    password text NOT NULL,
     email email,
+    password text NOT NULL,
     game_state JSON NOT NULL
 );
--- clicker_backend/src/main.rs
+++ clicker_backend/src/main.rs
@@ -13,9 +13,10 @@ use axum_database_sessions::{
 };
 use dashmap::DashMap;
 use sqlx::{PgPool, Pool};
+use std::io::{BufReader, Read, Write};
 use std::sync::Arc;
-use std::net::SocketAddr;
-use std::time::{Duration, Instant};
+use std::{net::SocketAddr, time::Duration};
+use tokio::time::{Instant};
 use tower_http::cors::CorsLayer;
 
 mod game_events;
@@ -44,6 +45,39 @@ async fn main() {
         std::fs::write("../clicker_frontend/src/game_messages.ts", ts_module).unwrap();
     }
 
+    let state = Arc::new(DashMap::<i64, GameState>::new());
+
+    let key = std::fs::File::open("master-key")
+        .ok()
+        .and_then(|file| {
+            let mut reader = BufReader::new(file);
+            let mut buffer = Vec::new();
+            reader.read_to_end(&mut buffer).expect("could not read key");
+            Key::try_from(buffer.as_slice()).ok()
+        })
+        .unwrap_or_else(|| {
+            let key = Key::generate();
+            let file = std::fs::File::options()
+                .create(true)
+                .write(true)
+                .append(false)
+                .open("master-key");
+            if let Ok(mut file) = file {
+                file.write_all(key.master())
+                    .expect("could not write key to file");
+            }
+            key
+        });
+
+    let pool = connect_to_database().await.unwrap();
+    let session_config = AxumSessionConfig::default()
+        .with_table_name("test_table")
+        .with_key(key);
+
+    let session_store = AxumSessionStore::<AxumPgPool>::new(Some(pool.clone().into()), session_config);
+
+    //Create the Database table for storing our Session Data.
+    session_store.initiate().await.unwrap();
     // build our application with a route
     #[allow(unused_mut)]
         let mut app = Router::new()
@@ -65,6 +99,15 @@ async fn main() {
         .unwrap();
 }
 
+async fn connect_to_database() -> anyhow::Result<sqlx::Pool<sqlx::Postgres>> {
+    Ok(Pool::connect("postgresql://admin:clickerroyale@localhost:5432/admin").await?)
+}
+
+/// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
 async fn sign_up(
     AuthBasic((email, password)): AuthBasic,
     session: AxumSession<AxumPgPool>,
@@ -72,7 +115,7 @@ async fn sign_up(
     Extension(state): Extension<GlobalState>,
 ) -> StatusCode {
     match sqlx::query!(
-        "SELECT id FROM Player WHERE email = $1 AND password = $2;",
+        "SELECT id FROM Player WHERE email::text = $1 AND password = $2;",
         email,
         password
     )
@@ -89,7 +132,7 @@ async fn sign_up(
             };
             let game_state_value = serde_json::to_value(game_state.clone()).unwrap();
             match sqlx::query!(
-                "INSERT INTO Player (email, password, game_state) VALUES ($1, $2, $3) RETURNING id;",
+                "INSERT INTO Player (email, password, game_state) VALUES ($1::TEXT::CITEXT, $2, $3) RETURNING id;",
                 email,
                 password,
                 game_state_value
@@ -113,11 +156,6 @@ async fn sign_up(
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
-}
-
-/// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
 }
 
 async fn connect_game(ws: WebSocketUpgrade) -> Response {

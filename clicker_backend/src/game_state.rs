a213afd7b2006723a1b6118940cddac6ae18b0ad
ntn98 <schmalfuss@stud.hs-heilbronn.de>
2022-10-27 15:33:11 +0200

    Shovel Amount upgrade Implemented upgrade functionality for the shovel amount
    #LABTROYALC-71
    
    @author: Anton Schmalfuß

--- clicker_backend/src/game_state.rs
+++ clicker_backend/src/game_state.rs
@@ -6,6 +6,7 @@ pub struct GameState {
     pub ore: f64,
     pub depth: f64,
     pub multiplier: f64,
+    pub shovel_amount_level: i32,
     pub shovel_depth_level: i32,
 }
 
@@ -18,7 +19,8 @@ impl GameState {
 
     /// Use this Function for Frontend -> Backend event handling
     pub fn handle(&mut self, event: ClientMessages) -> ServerMessages{
-        let mut upgrade_cost = self.shovel_depth_level * 50;
+        let mut depth_upgrade_cost = self.shovel_depth_level * 50;
+        let mut amount_upgrade_cost = self.shovel_amount_level * 50;
         let auto_digger_price = 200;
         match event {
             ClientMessages::Mine => {
@@ -27,16 +29,22 @@ impl GameState {
                 ServerMessages::NewState { ore: self.ore as u64, depth: self.depth as u64}
             }
             ClientMessages:: UpgradeShovelDepth => {
-                if upgrade_cost as f64 <= self.ore {
-                    self.ore -= upgrade_cost as f64;
+                if depth_upgrade_cost as f64 <= self.ore {
+                    self.ore -= depth_upgrade_cost as f64;
                     self.shovel_depth_level += 1;
-                    ServerMessages::ShovelDepthUpgraded{success: true, new_level: self.shovel_depth_level, new_upgrade_cost: upgrade_cost as u64}
+                    ServerMessages::ShovelDepthUpgraded{success: true, new_level: self.shovel_depth_level, new_upgrade_cost: depth_upgrade_cost as u64}
                 } else {
-                    ServerMessages::ShovelDepthUpgraded{success: false, new_level: self.shovel_depth_level, new_upgrade_cost: upgrade_cost as u64}
+                    ServerMessages::ShovelDepthUpgraded{success: false, new_level: self.shovel_depth_level, new_upgrade_cost: depth_upgrade_cost as u64}
                 }
             }
-            ClientMessages::SignUp => {
-                ServerMessages::SignUp { signed_in: false }
+            ClientMessages:: UpgradeShovelAmount => {
+                if amount_upgrade_cost as f64 <= self.ore {
+                    self.ore -= amount_upgrade_cost as f64;
+                    self.shovel_amount_level += 1;
+                    ServerMessages::ShovelAmountUpgraded{success: true, new_level: self.shovel_amount_level, new_upgrade_cost: amount_upgrade_cost as u64}
+                } else {
+                    ServerMessages::ShovelAmountUpgraded{success: false, new_level: self.shovel_amount_level, new_upgrade_cost: amount_upgrade_cost as u64}
+                }
             }
             ClientMessages::StartAutomation => {
                 if self.ore as u64 >= auto_digger_price {
@@ -51,6 +59,6 @@ impl GameState {
     }
 
     pub fn new() -> Self {
-        GameState { ore: 0.0, depth: 0.0, multiplier: 0.0, shovel_depth_level: 1 }
+        GameState { ore: 0.0, depth: 0.0, multiplier: 0.0, shovel_amount_level: 1, shovel_depth_level: 1 }
     }
 }

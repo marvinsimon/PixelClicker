e34a75013dbb82a9b44a568633c47851c3ff404a
ntn98 <63919788+ntn98@users.noreply.github.com>
2022-11-29 12:15:16 +0100

    Update create_tables.sql
    Re-added PVP table

--- clicker_backend/postgres/create_tables.sql
+++ clicker_backend/postgres/create_tables.sql
@@ -1,9 +1,20 @@
-
 DROP TABLE IF EXISTS Player;
-CREATE TABLE  Player (
-    id BIGSERIAL PRIMARY KEY,
-    email text NOT NULL ,
-    password text NOT NULL,
-    game_state JSON NOT NULL,
-    timestamp BIGINT
+CREATE TABLE Player
+(
+    id         BIGSERIAL PRIMARY KEY,
+    email      text    NOT NULL,
+    password   text    NOT NULL,
+    game_state JSON    NOT NULL,
+    timestamp  BIGINT,
+    pvp_score  BIGINT,
+    is_online  BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE PVP
+(
+    id        BIGSERIAL PRIMARY KEY,
+    id_att    BIGINT NOT NULL,
+    id_def    BIGINT NOT NULL,
+    loot      FLOAT NOT NULL,
+    timestamp BIGINT NOT NULL
 );

0b70fe65e7ab1b545c1660bb4e186e99c6c78496
Marvin Simon <msimon1@stud.hs-heilbronn.de>
2023-01-13 00:56:01 +0100

    refactor: slowly beginning to move the routing functions into it´s own file author: Marvin

--- clicker_backend/src/main.rs
+++ clicker_backend/src/main.rs
@@ -1,16 +1,9 @@
 use std::time::Duration;
 
 use axum::{
-    Extension,
-    extract::ws::{Message, WebSocket, WebSocketUpgrade},
-    http::StatusCode,
-    response::Response,
+    extract::ws::{Message, WebSocket},
 };
-use axum::http::HeaderMap;
-use axum_auth::AuthBasic;
 use axum_database_sessions::{AxumPgPool, AxumSession};
-use regex::Regex;
-use rustrict::CensorStr;
 use sqlx::{PgPool, Pool, Postgres};
 use sqlx::types::chrono::Utc;
 use tokio::time::Instant;
@@ -18,9 +11,8 @@ use tokio::time::Instant;
 use crate::events::daily_event;
 use crate::game_messages::{ClientMessages, ServerMessages};
 use crate::game_state::GameState;
-use crate::password_management::{hash_password, verify_password};
 use crate::server::{create_session_table, start_server};
-use crate::sql_queries::{insert_pvp_data, load_game_state_from_database, pvp_resource_query, save_game_state_to_database, save_score_to_database, save_timestamp_to_database, search_for_enemy, set_player_as_offline, set_player_as_online, test_for_new_registry, write_state_diff_to_database};
+use crate::sql_queries::{insert_pvp_data, load_game_state_from_database, pvp_resource_query, save_game_state_to_database, save_score_to_database, save_timestamp_to_database,  test_for_new_registry };
 use crate::startup::{check_for_players, create_game_message_file_type_script, create_session_key};
 
 mod game_messages;
@@ -37,7 +29,6 @@ const PLAYER_AUTH: &str = "player-auth";
 
 #[tokio::main]
 async fn main() {
-
     let pool = connect_to_database().await.unwrap();
 
     create_game_message_file_type_script();
@@ -60,158 +51,6 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn login(
-    AuthBasic((email, password)): AuthBasic,
-    session: AxumSession<AxumPgPool>,
-    Extension(pool): Extension<PgPool>,
-) -> StatusCode {
-    match sqlx::query!(
-        "SELECT id, game_state, password, timestamp FROM player WHERE (email = $1 OR username = $1);",
-        email
-    )
-        .fetch_optional(&pool)
-        .await
-    {
-        Ok(Some(record)) => {
-            if !verify_password(record.password, password.unwrap().as_bytes()) {
-                return StatusCode::UNAUTHORIZED;
-            }
-            session.set(PLAYER_AUTH, record.id);
-            let mut game_state: GameState = serde_json::from_value(record.game_state).unwrap();
-            let elapsed_time = Utc::now().timestamp() - record.timestamp.unwrap();
-            let prev_ore = game_state.ore;
-            let prev_depth = game_state.depth;
-            if elapsed_time > SECONDS_DAY {
-                game_state.tick(SECONDS_DAY);
-            } else {
-                game_state.tick(elapsed_time * 10);
-            }
-            let ore_diff = game_state.ore - prev_ore;
-            let depth_diff = game_state.depth - prev_depth;
-            println!("STATE DIFF\n\
-            ore: {}\n\
-            depth: {}", ore_diff, depth_diff);
-            write_state_diff_to_database(record.id, ore_diff, depth_diff, &pool).await;
-            save_game_state_to_database(record.id, &game_state, &pool).await;
-            save_score(record.id, &game_state, &pool).await;
-            set_player_as_online(record.id, &pool).await;
-            StatusCode::OK
-        }
-        Ok(None) => StatusCode::UNAUTHORIZED,
-        Err(err) => {
-            println!("{}", err);
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
-}
-
-
-async fn sign_up(
-    AuthBasic((email, password)): AuthBasic, username: HeaderMap,
-    session: AxumSession<AxumPgPool>,
-    Extension(pool): Extension<PgPool>,
-) -> StatusCode {
-    match sqlx::query!(
-        "SELECT id FROM player WHERE email = $1;",
-        email
-    )
-        .fetch_optional(&pool)
-        .await
-    {
-        Ok(Some(_)) => StatusCode::BAD_REQUEST,
-        Ok(None) => {
-            let email_regex = Regex::new(r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([a-z0-9]+)*\.[a-z]{2,6})").unwrap();
-            let game_state = GameState::new();
-            let game_state_value = serde_json::to_value(&game_state).unwrap();
-            let extracted_username = username.get("Username").unwrap().to_str().unwrap();
-            let inappropriate: bool = extracted_username.is_inappropriate();
-            if email_regex.is_match(&email) && !inappropriate {
-                return match sqlx::query!(
-                "INSERT INTO player (email, username, password, game_state) VALUES ($1, $2, $3, $4) RETURNING id;",
-                email,
-                extracted_username,
-                hash_password(password.unwrap().as_bytes()),
-                game_state_value
-            )
-                    .fetch_one(&pool)
-                    .await
-                {
-                    Ok(r) => {
-                        session.set(PLAYER_AUTH, r.id);
-                        save_score(r.id, &game_state, &pool).await;
-                        set_player_as_online(r.id, &pool).await;
-                        StatusCode::OK
-                    }
-                    Err(err) => {
-                        println!("{}", err);
-                        StatusCode::INTERNAL_SERVER_ERROR
-                    }
-                };
-            }
-            StatusCode::NOT_ACCEPTABLE
-        }
-        Err(err) => {
-            println!("{}", err);
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
-}
-
-
-async fn logout(
-    session: AxumSession<AxumPgPool>,
-    Extension(pool): Extension<PgPool>,
-) {
-    if let Some(id) = session.get::<i64>(PLAYER_AUTH) {
-        save_timestamp_to_database(id, &pool).await;
-        set_player_as_offline(id, &pool).await;
-    }
-    println!("Logging out!");
-    session.remove(PLAYER_AUTH);
-}
-
-async fn connect_game(ws: WebSocketUpgrade, Extension(pool): Extension<PgPool>, session: AxumSession<AxumPgPool>) -> Response {
-    println!("Connected!");
-    ws.on_upgrade(move |socket| handle_game(socket, session, pool))
-}
-
-async fn attack(
-    session: AxumSession<AxumPgPool>,
-    Extension(pool): Extension<PgPool>,
-) -> StatusCode {
-    if let Some(id) = session.get::<i64>(PLAYER_AUTH) {
-        return match sqlx::query!(
-        "SELECT id FROM PVP WHERE id_att = $1;",
-        id
-    )
-            .fetch_optional(&pool)
-            .await {
-            Ok(Some(_)) => {
-                StatusCode::BAD_REQUEST
-            }
-            Ok(None) => {
-                let defender_id = search_for_enemy(id, &pool).await;
-                if defender_id == -1 {
-                    if (sqlx::query!(
-                "DELETE FROM PVP WHERE id_att = $1",
-                id
-            ).execute(&pool).await).is_ok() {
-                        println!("No match found!");
-                        return StatusCode::NO_CONTENT;
-                    }
-                } else {
-                    calculate_combat(id, defender_id, &pool).await;
-                }
-                StatusCode::OK
-            }
-            Err(err) => {
-                println!("{}", err);
-                StatusCode::INTERNAL_SERVER_ERROR
-            }
-        };
-    }
-    StatusCode::BAD_REQUEST
-}
 
 async fn handle_game(mut socket: WebSocket, session: AxumSession<AxumPgPool>, pool: PgPool) {
     let mut game_state = GameState::new();

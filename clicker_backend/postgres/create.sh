b243fb06a362e914c2300f09ff61e267b57b0265
Marvin Simon <msimon1@stud.hs-heilbronn.de>
2023-01-18 14:45:56 +0100

    test if pipline runs now

--- clicker_backend/postgres/create.sh
+++ clicker_backend/postgres/create.sh
@@ -2,6 +2,6 @@
 docker stop royal-db
 docker rm royal-db
 sql=$(cat create_tables.sql)
-docker run -d -p 5432:5432 --name royal-db -e POSTGRES_DB=royal-db -e POSTGRES_PASSWORD=clickerroyale -e POSTGRES_USER=admin postgres:14.5-alpine postgres
+docker run -d -p 5432:5432 --name royal-db -e POSTGRES_DB=royal-db -e POSTGRES_PASSWORD=clickerroyale -e POSTGRES_USER=admin postgres:14.5-alpine clicker_db
 sleep 5
 docker exec royal-db psql -U admin -d royal-db -c "$sql"
